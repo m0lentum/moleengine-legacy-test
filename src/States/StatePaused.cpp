fbaab65c59f856a60e58335af9a35e00c209e701
Mikael Myyrä <mikael.myyrae@gmail.com>
2017-08-21 18:02:51 +0300

    Changed state switch hotkey

--- src/States/StatePaused.cpp
+++ src/States/StatePaused.cpp
@@ -5,9 +5,9 @@ namespace tg
 {
    void StatePaused::continuousUpdate(const sf::Time &timeElapsed)
    {
-      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+      if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return))
       {
-	 m_manager->transitionTo("PLAYING");
+		  m_manager->transitionTo("PLAYING");
       }
    }
 
